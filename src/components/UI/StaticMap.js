3bcfe019ea9fcb0577962bc08a75e661450bfa41
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-11 16:43:57 -0500

    static map refresh

--- src/components/UI/StaticMap.js
+++ src/components/UI/StaticMap.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
@@ -8,6 +9,15 @@ const StaticMap = ({ latitude, longitude }) => {
     latitudeDelta: 0.0022,
     longitudeDelta: 0.0021,
   };
+
+  const [lastLat, setLastLat] = useState();
+  const [lastLng, setLastLlg] = useState();
+  useEffect(() => {
+    if (latitude !== lastLat) setLastLat(latitude);
+    if (longitude !== lastLng) setLastLlg(longitude);
+  }, [latitude, longitude, lastLat, lastLng]);
+
+  if (latitude !== lastLat || longitude !== lastLng) return null;
 
   return (
     <MapView
