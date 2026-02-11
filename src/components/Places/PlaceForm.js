635409cb1aa2d23d4e358f5cb8c3df1ef77ceab4
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-11 13:57:20 -0500

    update place form to show image and location pickers

--- src/components/Places/PlaceForm.js
+++ src/components/Places/PlaceForm.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Colors } from '../../constants/colors';
+import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 
 const PlaceForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -19,6 +21,8 @@ const PlaceForm = () => {
           onChangeText={changeTitleHandler}
         />
       </View>
+      <ImagePicker />
+      <LocationPicker />
     </ScrollView>
   );
 };
