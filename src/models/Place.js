572f36d428c3342e32e6e02bf37cde3eb4cbe5ba
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-11 11:41:29 -0500

    icon btn component and constants

---
+++ src/models/Place.js
@@ -0,0 +1,16 @@
+import { customAlphabet } from 'nanoid/non-secure';
+
+const nanoid = customAlphabet(
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  10,
+);
+
+export class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; // { lat: number, lng: number}
+    this.id = nanoid();
+  }
+}
