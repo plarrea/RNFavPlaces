405332b463ec6f648f107ea4e61c591e920dfaa0
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-11 16:44:13 -0500

    added geocode functions

--- src/utils/location.js
+++ src/utils/location.js
@@ -1,3 +1,5 @@
+import { geocodeAsync, reverseGeocodeAsync } from 'expo-location';
+
 const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_MAPS_API_KEY;
 
 export const getMapPreview = (lat, lng) => {
@@ -6,4 +8,48 @@ export const getMapPreview = (lat, lng) => {
   &markers=color:red%7Clabel:S%7C${lat},${lng}
   &key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
+};
+
+export const getAddressFromCoords = async (latitude, longitude) => {
+  try {
+    const result = await reverseGeocodeAsync({
+      latitude,
+      longitude,
+    });
+
+    if (result.length > 0) {
+      const address = result[0];
+
+      return {
+        name: address.name,
+        street: address.street,
+        streetNumer: address.streetNumber,
+        city: address.city,
+        region: address.region,
+        postalCode: address.postalCode,
+        country: address.country,
+        formatted: `${address.streetNumber ?? ''} ${address.street ?? ''}, ${address.city ?? ''}, ${address.region ?? ''}, ${address.country ?? ''}`,
+      };
+    }
+  } catch (error) {
+    console.error('Reverse geocoding failed:', error);
+  }
+};
+
+export const getCoordsFromAddress = async (address) => {
+  try {
+    const result = await geocodeAsync(address);
+
+    if (result.length > 0) {
+      return {
+        latitude: result[0].latitude,
+        longitude: result[0].longitude,
+      };
+    } else {
+      console.log('No results found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Geocoding failed:', error);
+  }
 };
